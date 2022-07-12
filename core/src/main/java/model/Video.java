afa7e69b60b375c762aacea2f07f52d8f5a13c25
mooatib <axel.raux@gmail.com>
2022-07-12 21:55:17 +0200

    Core: Create Video core model

---
+++ core/src/main/java/model/Video.java
@@ -0,0 +1,55 @@
+package model;
+
+import java.util.List;
+import java.util.Objects;
+
+public class Video {
+    int id;
+    String title;
+    List<String> labels;
+
+    public Video(String title, List<String> labels) {
+        this.title = title;
+        this.labels = labels;
+    }
+
+    public Video(int id, String title, List<String> labels) {
+        this.id = id;
+        this.title = title;
+        this.labels = labels;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public List<String> getLabels() {
+        return labels;
+    }
+
+    @Override
+    public String toString() {
+        return "Video{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", labels=" + labels +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Video video = (Video) o;
+        return Objects.equals(title, video.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title);
+    }
+}
